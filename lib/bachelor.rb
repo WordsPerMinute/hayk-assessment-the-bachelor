f46f4f37e116b3a3f9dcb93d51a34d18d181a761
WordsPerMinute <contact@coreyhodge.net>
2020-01-09 09:40:53 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,65 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  seasonWinner = ""
+  data[season].each do |person|
+    if person["status"] == "Winner"
+      return seasonWinner = person["name"].split(' ').first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  personOfOccupation = ""
+  data.each do |season, contestantHash|
+    contestantHash.each do |aContestant|
+      if aContestant["occupation"] == occupation
+        personOfOccupation = aContestant["name"]
+      end
+    end
+  end
+  return personOfOccupation
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  countTown = {}
+  data.each do |season, contestantHash|
+    contestantHash.each do |aContestant|
+      #check if hash has, or doesn't have, a key entry for hometown already
+      if aContestant["hometown"] == hometown
+        if countTown.has_key?(hometown) == false
+            countTown[hometown] = 1
+        else countTown.has_key?(hometown)
+            countTown[hometown] += 1
+        end
+      end
+    end
+  end
+  return countTown[hometown]
 end
 
 def get_occupation(data, hometown)
-  # code here
+  firstHometownerJob = ""
+  data.each do |season, contestantHash|
+    contestantHash.each do |aContestant|
+      if aContestant["hometown"] == hometown
+        firstHometownerJob = aContestant["occupation"]
+        return firstHometownerJob
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ageTotal = 0
+  contestantTotal = 0
+  data.each do |seasonNum, contestantHash|
+    if seasonNum == season
+      contestantHash.each do |person|
+        ageTotal += (person["age"]).to_f
+        contestantTotal += 1
+      end
+    end
+  end
+  average = (ageTotal/contestantTotal).round
 end
